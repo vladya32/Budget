cba46b2e468045bc3453d85f06bd04e3189e0be5
vladya32 <vladya32@i.ua>
2019-02-19 21:16:11 +0200

    add

--- js/index.js
+++ js/index.js
@@ -43,6 +43,10 @@ let UIController = (function() {
             };
         },
 
+        addListItem: function(obj, type) {
+            '<div class="item clearfix" id="income-0"> <div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
